b24a7e69b9e535b00700663fbd7c27fc5877cecd
elistjames <ehstjames@gmail.com>
2021-11-26 20:58:56 -0700

    ...

--- src/User/SearchCriteria.java
+++ src/User/SearchCriteria.java
@@ -10,6 +10,11 @@ public class SearchCriteria {
 
     public SearchCriteria(int renterID) {
         this.renterID = renterID;
+        type = "any";
+        n_bedrooms = -1;
+        n_bathrooms = -1;
+        furnished = -1;
+        cityQuadrant = "any";
     }
 
     public SearchCriteria(int renterID, String type, int n_bedrooms, int n_bathrooms, int furnished,
